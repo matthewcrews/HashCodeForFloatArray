fdc29977913d854f904fb1a2865e37e9ac92729f
Matthew <matthewcrews@gmail.com>
2021-11-06 11:44:14 -0700

    terrible hash

--- Program.fs
+++ Program.fs
@@ -10,9 +10,52 @@ type Settings_Default = {
     ValveStates : array<int>
 }
 
+[<CustomEquality; NoComparison>]
+type Settings_Terrible = {
+    Capacities : array<float>
+    MaxRates : array<float>
+    ValveStates : array<int>
+} with
+    override this.GetHashCode () =
+        let capacityHash =
+            let mutable acc = 1610612741
+            let mutable idx = 0
+            while idx < this.Capacities.Length do
+                acc <- acc ^^^ this.Capacities.[idx].GetHashCode ()
+                idx <- idx + 1
+
+            acc
+
+        let maxRatesHash =
+            let mutable acc = 1610612741
+            let mutable idx = 0
+            while idx < this.MaxRates.Length do
+                acc <- acc ^^^ this.MaxRates.[idx].GetHashCode ()
+                idx <- idx + 1
+
+            acc
+
+        let valveStatesHash =
+            let mutable acc = 1610612741
+            let mutable idx = 0
+            while idx < this.ValveStates.Length do
+                acc <- acc ^^^ this.ValveStates.[idx].GetHashCode ()
+                idx <- idx + 1
+
+            acc
+
+        capacityHash ^^^ maxRatesHash ^^^ valveStatesHash
+
+    override this.Equals b =
+        match b with
+        | :? Settings_Terrible as other ->
+            this.Capacities = other.Capacities
+            && this.MaxRates = other.MaxRates
+            && this.ValveStates = other.ValveStates
+        | _ -> false
 
 
-let rng = System.Random (123)
+let rng = Random (123)
 let maxCapacityValue = 1_000_000.0
 let maxRateValue = 100_000.0
 let maxValveStateValue = 10
@@ -22,7 +65,7 @@ let valveStateCount = 1_000
 let settingsCount = 1_000
 let iterationCount = 1_000
 
-
+// Generating our random test data
 let randomFloatArray (rng: Random) maxValue length =
     let result = Array.zeroCreate length
 
@@ -60,25 +103,82 @@ let valveStates =
             |> randomIntArray rng maxValveStateValue
     } |> Array.ofSeq
 
-let settings =
+
+// We want to make sure that all of our versions of the Settings type
+// have the same underlying data to hash and compare. This means we
+// need to compute the indices for the underlying data and use them
+// for all the versions of the Settings type we create.
+let valueIndexes =
     seq {
         for _ in 1 .. settingsCount ->
-        {
-            Capacities = capacities.[rng.Next (0, capacityCount)]
-            MaxRates = maxRates.[rng.Next (0, maxRateCount)]
-            ValveStates = valveStates.[rng.Next (0, valveStateCount)]
-        }
+        {|
+            CapacityIdx = rng.Next (0, capacityCount)
+            MaxRateIdx = rng.Next (0, maxRateCount)
+            ValveStateIdx = rng.Next (0, valveStateCount)
+        |}
     } |> Array.ofSeq
 
-let settingsLookup =
-    settings
+
+// These will be the indices for deciding which Settings values we
+// will look up in each of the dictionary. We want to ensure we are
+// looking up equivalent data in all the tests.
+let testIndexes =
+    seq {
+        for _ in 1 .. iterationCount ->
+            rng.Next (0, settingsCount)
+    } |> Array.ofSeq
+
+
+// This Settings type will use the default hash functionality built
+// into .NET
+let defaultSettings =
+    seq {
+        for vi in valueIndexes ->
+        {
+            Capacities = capacities.[vi.CapacityIdx]
+            MaxRates = maxRates.[vi.MaxRateIdx]
+            ValveStates = valveStates.[vi.ValveStateIdx]
+        } : Settings_Default
+    } |> Array.ofSeq
+
+
+// The dictionary for the Default Settings we will test lookup up values in
+let defaultSettingsLookup =
+    defaultSettings
     |> Seq.mapi (fun idx setting -> KeyValuePair (setting, idx))
     |> Dictionary
 
-let testLookups =
+
+// We create an array we will iterate through to lookup values in
+// the Dictionary. We lay it out in an array to provide the best
+// possible data locality in the loop.
+let defaultSettingsTestLookups =
     seq {
-        for _ in 1 .. iterationCount ->
-            settings.[rng.Next (0, settings.Length - 1)]
+        for idx in testIndexes ->
+            defaultSettings.[idx]
+    } |> Array.ofSeq
+
+
+// Data for Settings_Terrible
+let terribleSettings =
+    seq {
+        for vi in valueIndexes ->
+        {
+            Capacities = capacities.[vi.CapacityIdx]
+            MaxRates = maxRates.[vi.MaxRateIdx]
+            ValveStates = valveStates.[vi.ValveStateIdx]
+        } : Settings_Terrible
+    } |> Array.ofSeq
+
+let terribleSettingsLookup =
+    terribleSettings
+    |> Seq.mapi (fun idx setting -> KeyValuePair (setting, idx))
+    |> Dictionary
+
+let terribleSettingsTestLookups =
+    seq {
+        for idx in testIndexes ->
+            terribleSettings.[idx]
     } |> Array.ofSeq
 
 
@@ -92,9 +192,25 @@ type Benchmarks () =
         let mutable idx = 0
         let mutable result = 0
 
-        while idx < testLookups.Length do
-            let testKey = testLookups.[idx]
-            result <- settingsLookup.[testKey]
+        while idx < defaultSettingsTestLookups.Length do
+            let testKey = defaultSettingsTestLookups.[idx]
+            result <- defaultSettingsLookup.[testKey]
+
+            idx <- idx + 1
+
+        result
+
+    [<Benchmark>]
+    member _.TerribleHash () =
+        // Want a fresh RNG with a seed to ensure all versions use
+        // the same lookups.
+        let rng = Random (1337)
+        let mutable idx = 0
+        let mutable result = 0
+
+        while idx < terribleSettingsTestLookups.Length do
+            let testKey = terribleSettingsTestLookups.[idx]
+            result <- terribleSettingsLookup.[testKey]
 
             idx <- idx + 1
 
